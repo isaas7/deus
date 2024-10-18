35b57b2cf6548bee795ba31579797af93ca99880
isaas7 <diyorsattarov@outlook.com>
2024-10-18 17:43:09 -0500

    more nested template ideas

--- foo/src/tests/foo/src/test_main.cpp
+++ foo/src/tests/foo/src/test_main.cpp
@@ -1,122 +1,63 @@
 #include <iostream>
-// Macro for stack trace
-#define STACK_TRACE std::cerr << "Error at " << __FUNCTION__ << " (Line " << __LINE__ << ")\n"
-
-// Macro for architecture-specific code
-#if defined(__arm__) || defined(__aarch64__)
-    #define ARCHITECTURE "ARM"
-    using int32 = int32_t; // 32-bit integer type
-    using int64 = int64_t; // 64-bit integer type
-#elif defined(__i386__) || defined(__x86_64__)
-    #define ARCHITECTURE "Intel"
-    using int32 = int32_t; // 32-bit integer type
-    using int64 = int64_t; // 64-bit integer type
-#else
-    #define ARCHITECTURE "Unknown Architecture"
-    using int32 = int32_t; // Fallback to a standard 32-bit type
-    using int64 = int64_t; // Fallback to a standard 64-bit type
-#endif
+#include <tuple>
+#include <string>
+#include <utility>
 
 // Templated class Foobar
 template <typename T>
 class Foobar {
 public:
     Foobar(T value) : value_(value) {}
-    void print() const {
+    void show() const {
         std::cout << "Foobar value: " << value_ << std::endl;
-    }
-    void setRootPath(const std::string& rootPath) {
-        rootPath_ = rootPath;
-        std::cout << "Root path set to: " << rootPath_ << std::endl;
-    }
-    void generate(int numSrcFiles, int numIncFiles, int numTestFiles) {
-        std::cout << "Generating structure with " << numSrcFiles << " source files, " << numIncFiles << " header files, and " << numTestFiles << " test files." << std::endl;
     }
 private:
     T value_;
-    std::string rootPath_;
 };
 
-// Templated template class Barfoo
-template <template <typename> class TemplateClass, typename T>
-class Barfoo {
+// Parameterized Template Nesting Class
+// Accepts a list of template classes and nests them based on the number of nested levels specified
+template <typename T, template <typename> class... Templates>
+class NestedTemplates;
+
+// Base case: single template class
+template <typename T, template <typename> class SingleTemplate>
+class NestedTemplates<T, SingleTemplate> {
 public:
-    Barfoo(T value) : instance_(value) {}
+    NestedTemplates(T value) : instance_(value) {}
     void show() const {
         instance_.print();
     }
 private:
-    TemplateClass<T> instance_;
+    SingleTemplate<T> instance_;
 };
 
-// Templated template class BarFoobar
-// Modified to accept template arguments matching the intended template nesting usage
-template <template <typename> class OuterTemplateClass, template <typename> class InnerTemplateClass, typename T>
-class BarFoobar {
+// Recursive case: multiple template classes
+template <typename T, template <typename> class First, template <typename> class... Rest>
+class NestedTemplates<T, First, Rest...> {
 public:
-    BarFoobar(T value) : instance_(value) {}
-    void display() const {
+    NestedTemplates(T value) : instance_(value) {}
+    void show() const {
         instance_.show();
     }
 private:
-    OuterTemplateClass<InnerTemplateClass<T>> instance_;
+    First<NestedTemplates<T, Rest...>> instance_;
 };
-
-// Templated template class FoobarBarfoo
-// Modified to accept template arguments matching the intended template nesting usage
-template <template <typename> class OuterTemplateClass, template <typename> class MiddleTemplateClass, template <typename> class InnerTemplateClass, typename T>
-class FoobarBarfoo {
-public:
-    FoobarBarfoo(T value) : instance_(value) {}
-    void output() const {
-        instance_.display();
-    }
-private:
-    OuterTemplateClass<MiddleTemplateClass<InnerTemplateClass<T>>> instance_;
-};
-
-bool foo() {
-    Foobar<int> generator(0);  // Instantiate with int as the template type
-
-    // Print the architecture
-    std::cout << "Architecture: " << ARCHITECTURE << std::endl;
-
-    // Configure root path
-    std::string rootPath;
-    std::cout << "Enter root path: ";
-    std::cin >> rootPath;
-    generator.setRootPath(rootPath);
-
-    // Input for number of files
-    int numSrcFiles, numIncFiles, numTestFiles;
-    std::cout << "Enter number of source files: ";
-    std::cin >> numSrcFiles;
-    std::cout << "Enter number of header files: ";
-    std::cin >> numIncFiles;
-    std::cout << "Enter number of test files: ";
-    std::cin >> numTestFiles;
-
-    // Generate the structure
-    generator.generate(numSrcFiles, numIncFiles, numTestFiles);
-    return true;
-}
 
 bool foobar() {
-    Foobar<int> fooInstance(42);
-    Barfoo<Foobar, int> barInstance(42);
-    BarFoobar<Barfoo, Foobar, int> barfoobarInstance(42);
-    FoobarBarfoo<Barfoo, Barfoo, Foobar, int> foobarbarfooInstance(42);
+    // Instantiate nested template classes based on combinations of Foobar and nesting levels
+    NestedTemplates<int, Foobar> fooInstance(42);
+    NestedTemplates<int, Foobar, Foobar> doubleFooInstance(42);
+    NestedTemplates<int, Foobar, Foobar, Foobar> tripleFooInstance(42);
 
-    barInstance.show();
-    barfoobarInstance.display();
-    foobarbarfooInstance.output();
+    fooInstance.show();
+    doubleFooInstance.show();
+    tripleFooInstance.show();
 
     return true;
 }
 
 int main() {
-    // Uncomment one of the below lines depending on the desired functionality
-    //return foo();
     return foobar();
 }
 
